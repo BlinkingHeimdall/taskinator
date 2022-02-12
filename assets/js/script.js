e1f1ec284ec182976f6e3ac22963a646890a08a0
Jason Adams <jmadams41@gmail.com>
2022-02-12 15:11:39 -0700

    adding local storage abilities

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
+var tasks = [];
 
 var  taskFormHandler = function(event) {
     event.preventDefault();
@@ -34,9 +35,10 @@ if (isEdit) {
   // no data attribute, so create object as normal and pass to createTaskEl function
 else {
     var taskDataObj = {
-    name: taskNameInput,
-    type: taskTypeInput
-    };
+        name: taskNameInput,
+        type: taskTypeInput,
+        status: "to do"
+    }
 
     createTaskEl(taskDataObj);
 }
@@ -44,6 +46,9 @@ else {
     };
 
 var createTaskEl = function(taskDataObj) {
+
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
     
     // create list item
     var listItemEl = document.createElement("li");
@@ -62,6 +67,10 @@ var createTaskEl = function(taskDataObj) {
 
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
+
+    taskDataObj.id = taskIdCounter;
+
+    tasks.push(taskDataObj);
 
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
@@ -161,6 +170,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
 
+    // loop through tasks array and task object with new content
+for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === parseInt(taskId)) {
+    tasks[i].name = taskName;
+    tasks[i].type = taskType;
+    }
+    };
+
     alert("Task Updated!");
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
@@ -185,6 +202,14 @@ var taskStatusChangeHandler = function(event) {
         else if (statusValue === "completed") {
             tasksCompletedEl.appendChild(taskSelected);
         }
+
+        // update task's in tasks array
+    for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === parseInt(taskId)) {
+    tasks[i].status = statusValue;
+    }
+    }
+    console.log(tasks);
     };
 
 formEl.addEventListener("submit",  taskFormHandler);
